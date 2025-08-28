8b9564f18de1be4b98712639f79eaf2405cfa26b
Azar-02 <mailforazarudeen@gmail.com>
2025-08-28 19:05:43 +0530

    Update sql_queries.sql

---
+++ sql_queries.sql
@@ -0,0 +1,13 @@
+-- 1. Average price by brand
+
+
+-- 2. Top 5 most expensive cars
+
+
+-- 3. Average mileage by fuel type
+
+
+-- 4. Price vs. car age trend (average per year)
+
+
+-- 5. Most common drivetrain per brand
